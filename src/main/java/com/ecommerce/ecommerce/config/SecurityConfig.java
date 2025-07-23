25e5949fae68d94f24cb4370fbba87be29ade6e3
LeandroZacariaz <leandrozacariaz18@gmail.com>
2025-07-23 04:06:14 -0300

    modificando cors

--- src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:5173"
         ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "PATCH" ,"POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
--- src/main/java/com/ecommerce/ecommerce/config/WebConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173, https://tienda-jope.vercel.app/")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
